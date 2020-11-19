09872cc496b25bbbdf8da97e9b44814bd21f0728
AlexisCatrix <alexis.4x.ac@gmail.com>
2020-11-19 19:07:29 +0100

    Create MapPirateBay component to show leaflet map

---
+++ src/Components/MapPirateBay.jsx
@@ -0,0 +1,51 @@
+import { useState, useEffect } from "react";
+import { Map, Marker, TileLayer } from "react-leaflet";
+import { MapStyled } from "../ComponentsStyled/MapStyled";
+import L from "leaflet";
+import weatherData from "../weatherData.json";
+
+export default function MapPirateBay() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    getMock();
+  }, []);
+
+  const getMock = () => {
+    setData(weatherData.list);
+  };
+
+  return (
+    <div>
+      <h1>Hi pirate bay !</h1>
+
+      <div>
+        <MapStyled>
+          <Map center={[17.7, -83.6]} zoom={6} minZoom={0}>
+            <TileLayer
+              url="https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}"
+              attribution="Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC"
+            />
+            {data.map((dataMarker, index) => {
+              console.log(dataMarker);
+              return (
+                <Marker
+                  key={index}
+                  position={[dataMarker.coord.lat, dataMarker.coord.lon]}
+                  icon={L.icon({
+                    iconUrl: `http://openweathermap.org/img/wn/${dataMarker.weather[0].icon}@2x.png`,
+                    // shadowUrl: iconShadow,
+                    iconSize: [30, 42],
+                    iconAnchor: [15, 42],
+                    shadowAnchor: [12, 42],
+                    popupAnchor: [0, -40],
+                  })}
+                />
+              );
+            })}
+          </Map>
+        </MapStyled>
+      </div>
+    </div>
+  );
+}
