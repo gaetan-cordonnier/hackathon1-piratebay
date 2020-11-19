8fea556f004dc60780ea8483374c704dc08def74
Fx-Colliot <fxcolliot@gmail.com>
2020-11-19 23:31:59 +0100

    added header and header style

---
+++ src/ComponentsStyled/HeaderStyled.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  height: 20vh;
+  width: 100%;
+  background-color: black;
+`;
+export const BonesTitle = styled.h1`
+  @import url("http://fonts.cdnfonts.com/css/cartoon-bones");
+  font-family: "Cartoon Bones", sans-serif;
+  font-size: 10vh;
+  color: #a41d1d;
+`;
+
+export const Image = styled.img`
+  height: 15vh;
+  width: 8vw;
+`;
