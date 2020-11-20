5837132351e1d71a4594e5a28c0ede2dee41a7b7
AlexisCatrix <alexis.4x.ac@gmail.com>
2020-11-20 14:48:19 +0100

    Clean code for solve merging

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
+import { Reset } from "styled-reset";
+import { useState } from "react";
+import NotFound from "./Components/NotFound";
+import Header from "./Components/Header";
 import MapPirateBay from "./Components/MapPirateBay";
 import NotFound from "./Components/NotFound";
 import { Reset } from "styled-reset";
 
+import Footer from "./Components/Footer";
+
 function App() {
+  const [select, setSelect] = useState("weather");
+
+  const handleChange = (event) => {
+    setSelect(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <div>
+    <div className="App">
       <Reset />
       <NotFound />
+      <Header
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+        select={select}
+      />
+      <MapPirateBay select={select} />
+      <Footer />
+      <Reset />
     </div>
   );
 }
