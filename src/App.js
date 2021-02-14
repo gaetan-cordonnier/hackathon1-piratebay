38f980a8e179fdd278809a347b2be79aeef208d7
gaetan-cordonnier <gaetan.cordonnier@gmail.com>
2021-02-14 23:58:30 +0100

    Resolved confilcts

--- src/App.js
+++ src/App.js
@@ -1,41 +1,42 @@
-import { Reset } from "styled-reset";
 import { useState } from "react";
+import { Reset } from "styled-reset";
+import { Redirect, Route, Switch, BrowserRouter } from "react-router-dom";
 import NotFound from "./Components/NotFound";
-
 import Header from "./Components/Header";
 import MapPirateBay from "./Components/MapPirateBay";
-import NotFound from "./Components/NotFound";
-import { Reset } from "styled-reset";
-
-import Footer from "./Components/Footer";
-
 import Footer from "./Components/Footer";
 
 function App() {
-  const [select, setSelect] = useState("weather");
+	const [select, setSelect] = useState("weather");
 
-  const handleChange = (event) => {
-    setSelect(event.target.value);
-  };
+	const handleChange = (event) => {
+		setSelect(event.target.value);
+	};
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-  };
+	const handleSubmit = (event) => {
+		event.preventDefault();
+	};
 
-  return (
-    <div className="App">
-      <Reset />
-      <NotFound />
-      <Header
-        handleChange={handleChange}
-        handleSubmit={handleSubmit}
-        select={select}
-      />
-      <MapPirateBay select={select} />
-      <Footer />
-      <Reset />
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<div className="App">
+				<Reset />
+				<Switch>
+					<Route path="/">
+						<Header
+							handleChange={handleChange}
+							handleSubmit={handleSubmit}
+							select={select}
+						/>
+						<MapPirateBay select={select} />
+						<Footer />
+					</Route>
+					<Route path="/404" component={NotFound} />
+					<Redirect to="/404" />
+				</Switch>
+			</div>
+		</BrowserRouter>
+	);
 }
 
 export default App;
