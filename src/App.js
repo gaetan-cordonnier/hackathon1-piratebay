e7bcd84d66aa7f6d12061b6d147443d930d54f85
gaetan-cordonnier <gaetan.cordonnier@gmail.com>
2021-02-14 23:59:12 +0100

    Prettier command

--- src/App.js
+++ src/App.js
@@ -7,36 +7,36 @@ import MapPirateBay from "./Components/MapPirateBay";
 import Footer from "./Components/Footer";
 
 function App() {
-	const [select, setSelect] = useState("weather");
+  const [select, setSelect] = useState("weather");
 
-	const handleChange = (event) => {
-		setSelect(event.target.value);
-	};
+  const handleChange = (event) => {
+    setSelect(event.target.value);
+  };
 
-	const handleSubmit = (event) => {
-		event.preventDefault();
-	};
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
 
-	return (
-		<BrowserRouter>
-			<div className="App">
-				<Reset />
-				<Switch>
-					<Route path="/">
-						<Header
-							handleChange={handleChange}
-							handleSubmit={handleSubmit}
-							select={select}
-						/>
-						<MapPirateBay select={select} />
-						<Footer />
-					</Route>
-					<Route path="/404" component={NotFound} />
-					<Redirect to="/404" />
-				</Switch>
-			</div>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Reset />
+        <Switch>
+          <Route path="/">
+            <Header
+              handleChange={handleChange}
+              handleSubmit={handleSubmit}
+              select={select}
+            />
+            <MapPirateBay select={select} />
+            <Footer />
+          </Route>
+          <Route path="/404" component={NotFound} />
+          <Redirect to="/404" />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/Components/NotFound.jsx
+++ src/Components/NotFound.jsx
@@ -1,22 +1,22 @@
 import { Link } from "react-router-dom";
 import backgroundImage from "../images/notFoundPlayMobileResize.jpg";
 import {
-	BackImage,
-	MessageError,
-	Body,
-	ButtonReturn,
+  BackImage,
+  MessageError,
+  Body,
+  ButtonReturn,
 } from "../ComponentsStyled/NotFoundStyled";
 
 export default function NoteFound() {
-	return (
-		<>
-			<Body>
-				<MessageError>Boh! Did we get lost, little boy?</MessageError>
-				<Link to="/">
-					<ButtonReturn>Return safe place</ButtonReturn>
-				</Link>
-			</Body>
-			<BackImage src={backgroundImage} />
-		</>
-	);
+  return (
+    <>
+      <Body>
+        <MessageError>Boh! Did we get lost, little boy?</MessageError>
+        <Link to="/">
+          <ButtonReturn>Return safe place</ButtonReturn>
+        </Link>
+      </Body>
+      <BackImage src={backgroundImage} />
+    </>
+  );
 }
